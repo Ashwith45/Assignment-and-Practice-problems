434d83e7bf0ddeaaf42f319b84eb37f9a81fe100
Ashwith45 <ashwith0411@gmail.com>
2025-02-03 23:43:39 +0530

    All Oops

---
+++ Oops.java
@@ -0,0 +1,87 @@
+import java.util.Scanner;
+
+interface Details {
+    String director = "RajaMouli";
+    String musicComposer ="Keeravani";
+    String cinematographer = "Senthi Kumar";
+    public void movie();
+
+}
+ abstract class Crew implements Details {
+    private String script;
+    public void crewDetails(){
+        System.out.println("Name of The Director :" +director);
+        System.out.println("Name of The Music Composer :" +musicComposer);
+        System.out.println("Name of The CinematoGrapher :" +cinematographer);
+    
+        
+
+    }
+    private void script(){
+        System.out.println("By Clicking the Below link you can get the Script of this Movie : ");
+        System.out.println("www.moviescript.in");
+    }
+    public String getScript(){
+        return "script";
+    }
+    public void setScript(String draft){
+        this.script = draft;
+        System.out.println("If You need the " + script);
+    }
+    public void displayScriptDetails() {
+        script();
+        
+    }
+
+}
+class Bahubali extends Crew {
+    public void movie() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Hero Name of Movie Bahubali : ");
+        String Hero = sc.nextLine();
+        System.out.println("Crew Deatls of Bahubali Movie are :");
+        System.out.println("Name of the MainLead : "+Hero);
+        crewDetails();
+        getScript();
+        
+        
+    }
+}
+class RRR extends Crew{ 
+    public void movie(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Hero Names of Movie RRR : ");
+        String Hero1 = sc.nextLine();
+        String Hero2 = sc.nextLine();
+        System.out.println("Crew Deatls of RRR Movie are :");
+        System.out.println("Names of the MainLead : "+Hero1 + " and  "   +Hero2);
+        crewDetails();
+        getScript();
+        
+
+
+    }
+
+    
+}
+
+
+
+
+public class Oops {
+    public static void main(String[] args) {
+        Bahubali b= new Bahubali();
+        b.movie();
+        b.getScript();
+        b.setScript(" Script of Bahubali");
+        b.displayScriptDetails();
+        
+        RRR rrr= new RRR();
+        rrr.movie();
+        rrr.getScript();
+        rrr.setScript("Script of RRR");
+        rrr.displayScriptDetails();
+        
+    }
+    
+}
